20b3cdf0fc108f37453ce2ca9ed8edb39a843b3e
hyang80 <163344069+hyang80@users.noreply.github.com>
2024-05-27 15:38:23 +0900

    수업 진행

---
+++ src/w12/index/script/script_4.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let elemBody = document.body;
+  elemBody.style.backgroundColor = "#00ff00";
+
+  let container = document.querySelector(".container");
+  let cards = document.querySelectorAll(".card");
+
+  for (let i = 0; i < cards.length; i++) {
+    let eachCard = cards[i];
+    eachCard.dataset.nth = i + 1;
+    eachCard.addEventListener("click", function (evt) {
+      container.dataset.selected = evt.currentTarget.dataset.nth;
+    });
+  }
+});
